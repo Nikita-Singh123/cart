149e27d4f109ff6dfd1a95074e181267c42423f7
hgupta10 <himanshu.h.gupta@capgemini.com>
2020-01-20 13:17:47 +0530

    Updates

---
+++ precache-manifest.c67438186809ff0f96f9423af415cabe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffe7936c3959c110aa05456e104b8e2b",
+    "url": "/CartApplication/index.html"
+  },
+  {
+    "revision": "8f3f3ab5e7a9125735ad",
+    "url": "/CartApplication/static/css/2.9c502461.chunk.css"
+  },
+  {
+    "revision": "f34a90d23f6571e9ba9f",
+    "url": "/CartApplication/static/css/main.cb0c28ab.chunk.css"
+  },
+  {
+    "revision": "8f3f3ab5e7a9125735ad",
+    "url": "/CartApplication/static/js/2.e06730cb.chunk.js"
+  },
+  {
+    "revision": "78ddf5a465591a03b9601ba2ddd63415",
+    "url": "/CartApplication/static/js/2.e06730cb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f34a90d23f6571e9ba9f",
+    "url": "/CartApplication/static/js/main.27653ea6.chunk.js"
+  },
+  {
+    "revision": "b3bc6a68b9063953ceb5",
+    "url": "/CartApplication/static/js/runtime-main.bc00b4ea.js"
+  }
+]);
